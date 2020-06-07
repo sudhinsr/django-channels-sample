28321ae7e61e885efa2505f3f82c3acc37b06835
Sudhin <sudhinsureshr@gmail.com>
2020-06-07 14:46:39 +0530

    converted into sync to async websocket consumer

--- tutorial/quickstart/consumer.py
+++ tutorial/quickstart/consumer.py
@@ -1,27 +1,27 @@
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class NotificationConsumer(WebsocketConsumer):
+class NotificationConsumer(AsyncWebsocketConsumer):
 
-    def connect(self):
+    async def connect(self):
         self.room_name = "room"
         self.room_group_name = "notification_room"
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
-        async_to_sync(self.channel_layer.group_send) (
+    async def receive(self, text_data):
+        await self.channel_layer.group_send(
            self.room_group_name,
             {
                 'type': 'send_message',
@@ -29,9 +29,9 @@ class NotificationConsumer(WebsocketConsumer):
             }
         )
     
-    def send_message(self, event):
+    async def send_message(self, event):
         message = event['message']
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
